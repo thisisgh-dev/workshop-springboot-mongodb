b68c0a925447bfccbc6979cbfd44f6f8077ecf46
Guilherme | Web developer <mandeparaguilherme@gmail.com>
2019-03-08 00:07:27 -0300

    User input with POST

--- src/main/java/com/guilherme/workshopmongo/services/UserService.java
+++ src/main/java/com/guilherme/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.workshopmongo.domain.User;
+import com.guilherme.workshopmongo.dto.UserDTO;
 import com.guilherme.workshopmongo.repository.UserRepository;
 import com.guilherme.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		
 	}
 }
