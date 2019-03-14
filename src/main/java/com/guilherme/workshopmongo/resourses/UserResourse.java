2306e1c65ce3ab3980f0d459b3dc92f95c14276b
Guilherme | Web developer <mandeparaguilherme@gmail.com>
2019-03-13 21:16:24 -0300

    Endpoint to return a user's posts

--- src/main/java/com/guilherme/workshopmongo/resourses/UserResourse.java
+++ src/main/java/com/guilherme/workshopmongo/resourses/UserResourse.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.guilherme.workshopmongo.domain.Post;
 import com.guilherme.workshopmongo.domain.User;
 import com.guilherme.workshopmongo.dto.UserDTO;
 import com.guilherme.workshopmongo.services.UserService;
@@ -64,4 +65,10 @@ public class UserResourse {
 		
 		
 	}	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET) //or @GetMapping
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+		
+	}
 }
